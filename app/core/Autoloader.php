ce967f9d1824c623f8bcba608e436d355a135df9
fares21 <bouaffar00@gmail.com>
2025-10-19 22:40:11 +0100

    Create Autoloader.php

---
+++ app/core/Autoloader.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register(function ($class) {
+    // أسماء ضمنية: App... -> app/...
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../';
+    if (strncmp($prefix, $class, strlen($prefix)) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, strlen($prefix));
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
