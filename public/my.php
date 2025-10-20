a5a1c17a323abf99edbe4864af9781a5a1a13784
fares21 <bouaffar00@gmail.com>
2025-10-20 20:07:32 +0100

    Create my.php

---
+++ public/my.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+$cfg = include __DIR__ . '/../config/config.php';
+require __DIR__ . '/../app/core/Autoloader.php';
+
+use App\core\Helpers;
+use App\controllers\TrackController;
+
+Helpers::langInit($cfg);
+
+$controller = new TrackController($cfg);
+$controller->myList();
