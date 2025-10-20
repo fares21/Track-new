cc10685b6ae36b447d986bb6b318297b9d883722
fares21 <bouaffar00@gmail.com>
2025-10-20 20:08:14 +0100

    Create cron.php

---
+++ public/cron.php
@@ -0,0 +1,10 @@
+<?php
+// GET /cron.php?secret=XXXX
+session_start();
+$cfg = include __DIR__ . '/../config/config.php';
+require __DIR__ . '/../app/core/Autoloader.php';
+
+use App\controllers\TrackController;
+
+$controller = new TrackController($cfg);
+$controller->cron();
